fcbfaa4a52141ed05934cdf50984c45f6d24a821
shalam <shlmv92@gmail.com>
2023-05-09 22:04:00 +0300

    Added first_letter_up function

--- str_func.py
+++ str_func.py
@@ -1,4 +1,17 @@
 def word_up():
+    '''Ввод слова для функции'''
     print("Ведите слово:")
     user_word = input()
     return user_word.upper()
+
+
+def first_letter_up():
+    '''Запрос фразы'''
+    print("Введите фразу:")
+    '''Ввод фразы'''
+    user_input = input()
+    '''Переводим строку в список'''
+    user_input_split = user_input.split()
+    for n in user_input_split:
+        '''выводим элементы списка, каждый с загл. буквы'''
+        print(n.title(), end=' ')
